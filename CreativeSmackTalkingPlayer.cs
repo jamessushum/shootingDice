155224a697974f106a087770b8ecdeace0b3f530
James Su-Shum <sushumjames@gmail.com>
2020-09-03 11:53:05 -0400

    Started exercise requirements for CreativeSmackTalkingPlayer

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        List<string> listOfInsults = new List<string>()
+        {
+            "Go home, loser!",
+            "Is that the best you got?",
+            "Can I play someone better?",
+            "Are we still on the practice round?"
+        };
 
+        public override int Roll()
+        {
+
+        }
     }
 }
